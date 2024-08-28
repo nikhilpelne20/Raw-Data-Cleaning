23aa047a011b1a7f404f5882b119f8e6eb212aea
nikhil pelne <nikhil788p@gmail.com>
2024-08-28 20:55:58 +0530

    clean the author and narrator col

--- data_clean.py
+++ data_clean.py
@@ -7,14 +7,19 @@ def main():
     spark = spark_start('data_clean_etl')
     audible_data = r'D:\DataCleaning\Raw-Data-Cleaning\raw_data\audible\audible_uncleaned.csv'
     audible_df = extract_data(spark,audible_data)
-    #check the data frame
-    audible_df.show()
+    data_cleaning(audible_df)
 
 def extract_data(spark, path):
     df = spark.read.format('csv')\
         .option("header", True)\
         .load(path)
     return df
+
+def data_cleaning(df):
+    print("Inside data_cleaning function")
+    df_cleaned = df.withColumn("author", regexp_replace("author", r"^Writtenby:",""))\
+                    .withColumn("narrator", regexp_replace("narrator", r"^Narratedby:",""))
+    df_cleaned.show(truncate=False)
 
 # entry point for PySpark ETL application
 if __name__ == '__main__':
